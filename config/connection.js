f3cee6829ef671f52f87a33b9e75c096f34b35d0
Andrewy2416 <andrewy2416@gmail.com>
2022-02-21 23:20:56 -0800

    changed db name

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306,
-        database: 'newsfeed_db'
+        database: 'technews_db'
     });
 }
 
